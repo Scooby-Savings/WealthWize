1cc28feb5c9bca24025610d9a2cfd38a762b2365
StephyAllezy <lilianna.goforward@gmail.com>
2023-07-24 21:10:32 -0400

    added remove goal front end

--- frontend/src/pages/goalForm.js
+++ frontend/src/pages/goalForm.js
@@ -1,18 +1,34 @@
-import React, { useState, useContext } from "react";
-import "./goalForm.css";
-import { AuthContext } from "../authContext";
-import axios from "axios";
+import React, { useState, useContext } from 'react';
+import './goalForm.css';
+import { AuthContext } from '../authContext';
+import axios from 'axios';
 
 function GoalForm({ setSidebar }) {
   const auth = useContext(AuthContext);
-  const [goal, setGoal] = useState("");
-  const [amount, setAmount] = useState("");
+  const [goal, setGoal] = useState('');
+  const [amount, setAmount] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault;
     try {
       const response = await axios.post(
-        "http://localhost:3000/dashboard/savegoal",
+        'http://localhost:3000/dashboard/savegoal',
+        {
+          user_id: auth.userID,
+          goal,
+          amount,
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    setSidebar(false);
+  };
+  const handleSubmit2 = async (event) => {
+    event.preventDefault;
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/dashboard/savegoal',
         {
           user_id: auth.userID,
           goal,
@@ -33,12 +49,20 @@ function GoalForm({ setSidebar }) {
   };
 
   return (
-    <form className="goalForm" onSubmit={handleSubmit}>
-      <label>Add a goal</label>
-      <input placeholder="Goal Title" onChange={handleGoalChange} />
-      <input placeholder="Amount" onChange={handleAmountChange} />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form className='goalForm' onSubmit={handleSubmit}>
+        <label>Add a goal</label>
+        <input placeholder='Goal Title' onChange={handleGoalChange} />
+        <input placeholder='Amount' onChange={handleAmountChange} />
+        <button type='submit'>Submit</button>
+      </form>
+      <br></br>
+      <form className='goalForm' onSubmit={handleSubmit2}>
+        <label>Remove a goal</label>
+        <input placeholder='Goal Title' onChange={handleGoalChange} />
+        <button type='submit'>Remove</button>
+      </form>
+    </>
   );
 }
 
